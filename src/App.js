9def55286e9fbff5536a8d58d33db8fcd57f011f
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-07-09 17:11:46 +0200

    plz

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {useCollectionData} from 'react-firebase-hooks/firestore'
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCtLJzVyITIJYoAEPGr0QLIW6543R_lyRk",
+  authDomain: "jing-chat.firebaseapp.com",
+  projectId: "jing-chat",
+  storageBucket: "jing-chat.appspot.com",
+  messagingSenderId: "761884465848",
+  appId: "1:761884465848:web:cbe70ea645bf9f8a78f24d",
+  measurementId: "G-5HJTE9009D"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
